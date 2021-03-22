118296a9337c3fc546f57cab9daf408ba46c7a00
Willian Lopes <willianlopes404@gmail.com>
2021-03-22 13:50:17 -0300

    minor fix

--- slides/homeSlide1.js
+++ slides/homeSlide1.js
@@ -2,8 +2,7 @@
 import styles from '../styles/main.module.css'
 
 function HomeSlide1() {
-    return <div className={`${styles.slide} ${styles.slideBGGreen}`}>
-        Slide 1
+    return <div className={`${styles.slide} ${styles.slideBGGreen}`}>        
         <img src="https://museudapessoa.org/wp-content/uploads/2020/06/HMM_HV009_FT001-e1595559316475.png"
             className={styles.homeSlide1Img}/>
     </div>    
